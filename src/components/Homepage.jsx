d88d56cbd37a722824a4a4ce20d04bc91a6c4e6e
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2023-07-01 18:16:33 +0530

    [ADDED] Homepage theme

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className="flex flex-col font-sans w-full h-screen" style={{
+        background: "linear-gradient(0deg , rgba(16, 0, 43, 1) 50%, #000000 90%)", backdrop: "blur(4px)"
+        }}>
+    </div>
+  )
+}
+
+export default Homepage
